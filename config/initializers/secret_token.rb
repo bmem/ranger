abd4e763251d849ff4b8936435e4e49513b2d61f
Trevor Stone <github@trevorstone.org>
2012-02-11 13:57:45 -0700

    rails new ranger

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ranger::Application.config.secret_token = 'a550ebfae90f8e66ae7e456ecb90da92c838375204b9c3472d392f0fa27bd5c498af76da2c1f0d7babf7f515122dc6a83484340832ec0f2660d01dee1daf6c3d'
