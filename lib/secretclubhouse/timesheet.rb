c323517217a0a216ea34310d739889858bcc8ba5
Trevor Stone <github@trevorstone.org>
2012-12-10 21:13:46 -0700

    Adjust time zone for converted work logs

--- lib/secretclubhouse/timesheet.rb
+++ lib/secretclubhouse/timesheet.rb
@@ -5,11 +5,18 @@ module SecretClubhouse
     self.target ::WorkLog, :position_id, :start_time, :end_time
 
     def start_time
-      on_duty
+      adjust_time on_duty
     end
 
     def end_time
-      off_duty
+      adjust_time off_duty
+    end
+
+    def adjust_time(datetime)
+      # clubhouse stored "local" dates in UTC
+      # Burning Man happens during DST, manual adjustments in January
+      delta = datetime.month.in?(4..10) ? 7.hours : 8.hours
+      Time.zone.at(datetime.to_i + delta)
     end
   end
 end
