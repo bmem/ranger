8be6ce1b3a9c61d0a3c222626aa1f39e8e00f898
Trevor Stone <github@trevorstone.org>
2012-12-15 11:17:47 -0700

    Use string, not hash for assert message

--- test/unit/user_role_test.rb
+++ test/unit/user_role_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UserRoleTest < ActiveSupport::TestCase
   test "can add valid role" do
     ur = UserRole.new :user_id => 1, :role => 'admin'
-    assert ur.valid?, ur.errors.to_hash
+    assert ur.valid?, ur.errors.full_messages.join(', ')
   end
 
   test "can't add invalid role" do
