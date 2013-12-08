2bafafdb4740f16ad97c09b1bd409ed6e776d5ec
Trevor Stone <github@trevorstone.org>
2013-12-08 12:46:04 -0700

    Make home view useful

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,26 @@ class HomeController < ApplicationController
   skip_load_and_authorize_resource
 
   def index
+    if current_user
+      @person = current_user.person
+      select_interesting_events
+      respond_to do |format|
+        format.html { render action: :loggedin}
+      end
+    else
+      respond_to do |format|
+        format.html { render action: :loggedout}
+      end
+    end
+  end
+
+  private
+  def select_interesting_events
+    @interesting_events = []
+    @interesting_events += Event.current
+    @interesting_events += Event.upcoming
+    @interesting_events += Event.completed.limit(2)
+    default_event_id.try {|id| @interesting_events << Event.find(id)}
+    @interesting_events.uniq!
   end
 end
