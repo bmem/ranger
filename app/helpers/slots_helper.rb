63bc02bf5d95e5904c065091d602c1a79bd61675
Trevor Stone <github@trevorstone.org>
2014-02-11 00:17:44 -0700

    Show sign up meter on shift page

--- app/helpers/slots_helper.rb
+++ app/helpers/slots_helper.rb
@@ -1,2 +1,9 @@
 module SlotsHelper
+  def slot_meter_tag(slot)
+    num = slot.involvements.count
+    max = slot.max_people > 0 ? slot.max_people : 1000
+    min = slot.min_people
+    opt = slot.max_people > 0 ? slot.max_people : slot.min_people * 2
+    tag 'meter', {value: num, min: 0, low: min, max: max, optimum: opt}
+  end
 end
