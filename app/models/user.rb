8ad73b83a3d3a9bb695baa794c76b03033449bc9
Trevor Stone <github@trevorstone.org>
2014-03-01 00:02:28 -0700

    Disable new user registration until we have a firmer plan

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,9 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, #:registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  # TODO restore :registerable when we have firm plans about new volunteer flow
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me,
