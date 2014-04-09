1dd8181cbdbd00cb099e827deaaa740600f3f8e0
Trevor Stone <github@trevorstone.org>
2014-04-08 23:51:17 -0600

    Action to copy a bunch of folks to an event for testing

--- app/controllers/testing_controller.rb
+++ app/controllers/testing_controller.rb
@@ -29,4 +29,46 @@ class TestingController < ApplicationController
       format.json {render json: {roles: current_user.roles, masked_roles: current_user.masked_roles}}
     end
   end
+
+  # GET /populate_event
+  def populate_event
+    # TODO make a TestingPolicy
+    raise Pundit::NotAuthorizedError unless current_user.has_role? Role::ADMIN
+    @events = policy_scope(Event)
+    respond_to do |format|
+      format.html # populate_event.html.haml
+    end
+  end
+
+  # POST /update_populate_event
+  def update_populate_event
+    # TODO make a TestingPolicy
+    raise Pundit::NotAuthorizedError.new('Must be an admin') unless current_user.has_role? Role::ADMIN
+    source = Event.find(params[:source_event_id])
+    destination = Event.find(params[:destination_event_id])
+    raise 'Must specify different events' unless source != destination
+    existing = destination.person_ids.to_set
+    involvements = source.involvements
+    selected_array_param(params[:involvement_status]).presence.try do |statuses|
+      involvements = involvements.where(involvement_status: statuses)
+    end
+    created = 0
+    Involvement.transaction do
+      involvements.includes(:person).each do |sourceinv|
+        p = sourceinv.person
+        unless p.id.in? existing
+          inv = destination.involvements.build name: p.display_name,
+            personnel_status: p.status, involvement_status: 'planned',
+            barcode: p.barcode, camp_location: sourceinv.camp_location,
+            emergency_contact_info: sourceinv.emergency_contact_info
+          inv.person = p
+          inv.save!
+          created += 1
+        end
+      end
+    end
+    respond_to do |format|
+      format.html {redirect_to testing_populate_event_path, notice: "Added #{created} #{'person'.pluralize(created)} to #{destination}"}
+    end
+  end
 end
